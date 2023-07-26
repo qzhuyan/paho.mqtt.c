a8cddf7ce6ed18f400962fba2103cff1fef13a51
William Yang <mscame@gmail.com>
2023-07-26 09:57:12 +0200

    chore: update mqtt_over_quic.lua for instruction

--- test/mqtt_over_quic.lua
+++ test/mqtt_over_quic.lua
@@ -1,6 +1,9 @@
 -- Wireshark LUA script to add MQTT over QUIC support
 -- run:
 --   wireshark -X lua_script:mqtt_over_quic.lua
+--   OR put it in
+--      Unix-like:  ~/.local/lib/wireshark/plugins
+--      Windows: %APPDATA%\Wireshark\plugins
 local dissector_table = DissectorTable.get("quic.proto")
 local mqtt_handle = Dissector.get("mqtt")
 dissector_table:add("mqtt", mqtt_handle)
